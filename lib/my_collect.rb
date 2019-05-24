7b5a9c34e3428351e14725a2131d4b4f5bc4d413
James Gomez <james.gomez320@yahoo.com>
2019-05-24 01:49:50 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,18 @@
-def my_collect(collection)
-  stored_array = []
-  x = 0 
-  while x < collection.size do |x|
-    stored_array << yield([x])
-    x +=0 
-end 
+list = ["Tim Jones", "Bob Costas", "Don Knotts"]
+
+
+def my_collect(array)
+  i = 0
+  name_collection = []
+  while i < array.length
+    name_collection.push yield(array[i])
+  # you could also do it this way:
+  # name_collection << yield(array[i])
+    i += 1
+  end
+  name_collection
+end
+
+my_collect(list) {|i| i.split(" ").first}
     
 
